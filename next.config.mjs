57e6dfb82be750d969b23a10251173b9cfc87037
Javohir <yusupovjavoxir11@gmail.com>
2024-11-07 11:51:55 +0500

    new version uicgames

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // bu yerda konfiguratsiya sozlamalaringizni kiriting
+  turbo: true, // Turbopack o'chiriladi va Webpack ishlatiladi
+};
+
+export default nextConfig;
