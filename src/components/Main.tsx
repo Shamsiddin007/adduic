60e2f107cc79c0851ba90d77af8005cdc770f4e7
Shamsiddin007 <shamsiddinruziboyev13@gmail.com>
2024-11-08 14:41:24 +0500

    White

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,20 +1,38 @@
 import Image from "next/image";
 import Fire from "/public/icons/olov.png";
-import soldier from '/public/Imgs/askar.svg';
+import soldier from "/public/Imgs/askar.svg";
 
 export default function Mani() {
   return (
-    <div className="bg-black">
+    <div className="bg-white">
       <div className="container">
-        <div className="flex relative top-0 left-0">
+        <div className="flex">
           <div className="pt-40">
             <span className="flex gap-2 items-center bg-[#202123] w-[380px] rounded-full text-white text-sm py-2 px-3">
-              <Image src={Fire} alt="FireLogo" className="drop-shadow-custom-yellow"/> working day & night to bring
-              out the best results
+              <Image
+                src={Fire}
+                alt="FireLogo"
+                className="drop-shadow-custom-yellow"
+              />{" "}
+              working day & night to bring out the best results
             </span>
-            <h1 className="text-6xl font-bold uppercase  pt-[19px] text-white">
-              Your <span className="text-[#2760D0]">ideas</span> will <br />{" "}
-              become a <span className="text-[#2760D0]">reality</span>
+            <h1 className="text-6xl font-bold uppercase pt-[19px] text-white tracking-wider">
+              YOUR {" "}
+              <span className="relative text-blue-500 border border-blue-500 inline-block mb-2">
+                IDEAS
+                <span className="absolute w-2 h-2 bg-white rounded-full -top-1 -left-1"></span>
+                <span className="absolute w-2 h-2 bg-white rounded-full -top-1 -right-1"></span>
+                <span className="absolute w-2 h-2 bg-white rounded-full -bottom-1 -left-1"></span>
+                <span className="absolute w-2 h-2 bg-white rounded-full -bottom-1 -right-1"></span>
+              </span>
+              {" "} WILL <br /> BECOME A {" "}
+              <span className="relative text-blue-500 border border-blue-500 inline-block">
+                REALITY
+                <span className="absolute w-2 h-2 bg-white rounded-full -top-1 -left-1"></span>
+                <span className="absolute w-2 h-2 bg-white rounded-full -top-1 -right-1"></span>
+                <span className="absolute w-2 h-2 bg-white rounded-full -bottom-1 -left-1"></span>
+                <span className="absolute w-2 h-2 bg-white rounded-full -bottom-1 -right-1"></span>
+              </span>
             </h1>
             <p className="text-white text-base pb-14 pt-52">
               UIC Games is a game development company founded in 2018. It
@@ -24,12 +42,11 @@ export default function Mani() {
               gaming experiences
             </p>
           </div>
-
           <Image
             src={soldier}
             alt="soldierImage"
             width={parseInt("600px")}
-            className="absolute -right-44 -top-16"
+            className="sticky bottom-0 -mr-56"
           />
         </div>
       </div>
