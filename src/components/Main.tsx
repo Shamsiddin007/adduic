381dc2001712fa32b15e9510d648874b30999a72
Shamsiddin007 <shamsiddinruziboyev13@gmail.com>
2024-11-08 21:59:51 +0500

    ChangeMainpage

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,53 +1,47 @@
 import Image from "next/image";
 import Fire from "/public/icons/olov.png";
-import soldier from "/public/Imgs/askar.svg";
+import styles from "./style.module.css";
 
 export default function Mani() {
   return (
-    <div className="bg-white">
+    <div className="relative">
+      <div className={`${styles.solder}`}></div>
+      <div className="w-[55%] h-[55%] absolute -z-2 bottom-[25px] right-[190px] opacity-[70%]  rounded-full rounded-bl-none rounded-br-none bg-custom-radial"></div>
       <div className="container">
-        <div className="flex">
-          <div className="pt-40">
-            <span className="flex gap-2 items-center bg-[#202123] w-[380px] rounded-full text-white text-sm py-2 px-3">
-              <Image
-                src={Fire}
-                alt="FireLogo"
-                className="drop-shadow-custom-yellow"
-              />{" "}
-              working day & night to bring out the best results
+        <div className="pt-36">
+          <span className="flex gap-2 items-center bg-[#202123] w-[380px] rounded-full text-white text-sm py-2 px-3 font-sans">
+            <Image
+              src={Fire}
+              alt="FireLogo"
+              className="drop-shadow-custom-yellow"
+            />{" "}
+            Working day & night to bring out the best results
+          </span>
+          <h1 className="text-6xl font-bold uppercase pt-[19px] text-white tracking-wider">
+            YOUR{" "}
+            <span className="relative text-blue-500 border border-blue-500 inline-block mb-2">
+              IDEAS
+              <span className="absolute w-2 h-2 bg-white rounded-full -top-1 -left-1"></span>
+              <span className="absolute w-2 h-2 bg-white rounded-full -top-1 -right-1"></span>
+              <span className="absolute w-2 h-2 bg-white rounded-full -bottom-1 -left-1"></span>
+              <span className="absolute w-2 h-2 bg-white rounded-full -bottom-1 -right-1"></span>
+            </span>{" "}
+            WILL <br /> BECOME A{" "}
+            <span className="relative text-blue-500 border border-blue-500 inline-block">
+              REALITY
+              <span className="absolute w-2 h-2 bg-white rounded-full -top-1 -left-1"></span>
+              <span className="absolute w-2 h-2 bg-white rounded-full -top-1 -right-1"></span>
+              <span className="absolute w-2 h-2 bg-white rounded-full -bottom-1 -left-1"></span>
+              <span className="absolute w-2 h-2 bg-white rounded-full -bottom-1 -right-1"></span>
             </span>
-            <h1 className="text-6xl font-bold uppercase pt-[19px] text-white tracking-wider">
-              YOUR {" "}
-              <span className="relative text-blue-500 border border-blue-500 inline-block mb-2">
-                IDEAS
-                <span className="absolute w-2 h-2 bg-white rounded-full -top-1 -left-1"></span>
-                <span className="absolute w-2 h-2 bg-white rounded-full -top-1 -right-1"></span>
-                <span className="absolute w-2 h-2 bg-white rounded-full -bottom-1 -left-1"></span>
-                <span className="absolute w-2 h-2 bg-white rounded-full -bottom-1 -right-1"></span>
-              </span>
-              {" "} WILL <br /> BECOME A {" "}
-              <span className="relative text-blue-500 border border-blue-500 inline-block">
-                REALITY
-                <span className="absolute w-2 h-2 bg-white rounded-full -top-1 -left-1"></span>
-                <span className="absolute w-2 h-2 bg-white rounded-full -top-1 -right-1"></span>
-                <span className="absolute w-2 h-2 bg-white rounded-full -bottom-1 -left-1"></span>
-                <span className="absolute w-2 h-2 bg-white rounded-full -bottom-1 -right-1"></span>
-              </span>
-            </h1>
-            <p className="text-white text-base pb-14 pt-52">
-              UIC Games is a game development company founded in 2018. It
-              focuses on creating engaging and immersive video games,
-              <br />
-              combining technology with creative storytelling to deliver unique
-              gaming experiences
-            </p>
-          </div>
-          <Image
-            src={soldier}
-            alt="soldierImage"
-            width={parseInt("600px")}
-            className="sticky bottom-0 -mr-56"
-          />
+          </h1>
+          <p className="text-white font-sans text-base pb-14 pt-52">
+            UIC Games is a game development company founded in 2018. It focuses
+            on creating engaging and immersive video games,
+            <br />
+            combining technology with creative storytelling to deliver unique
+            gaming experiences
+          </p>
         </div>
       </div>
     </div>
