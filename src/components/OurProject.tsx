2ab7d519fd0c2cc406941baab683b4dae5c5f4d1
Odilhon13 <odilhonsayfullayev@gmail.com>
2024-11-28 12:20:57 +0500

    new push origin content

--- src/components/OurProject.tsx
+++ src/components/OurProject.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import Link from "next/link";
@@ -15,11 +16,12 @@ function ProjectsPage() {
 
   useEffect(() => {
     fetch(
-      "https://leuscgqzalmrfujkzpbd.supabase.co/storage/v1/object/sign/ourproject/singleImg/projects.json?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJvdXJwcm9qZWN0L3NpbmdsZUltZy9wcm9qZWN0cy5qc29uIiwiaWF0IjoxNzMxMjYzNDQ0LCJleHAiOjE3NjI3OTk0NDR9.LR1i1jdY-dZ1LUdenb9T8oKsJa_dtJNQ6pcmg2njGZM&t=2024-11-10T18%3A30%3A45.640Z"
+      "https://leuscgqzalmrfujkzpbd.supabase.co/storage/v1/object/sign/ourproject/allnews/news.json?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJvdXJwcm9qZWN0L2FsbG5ld3MvbmV3cy5qc29uIiwiaWF0IjoxNzMyNzAzMzQ4LCJleHAiOjE3NjQyMzkzNDh9.vyvNLVHppdjazVI-uLHevwXDm1UHQikiT84k9E5WnCM&t=2024-11-27T10%3A29%3A08.268Z"
     )
       .then((response) => response.json())
       .then((data) => {
-        const limitedProjects = data.ourprojects.slice(0, 4);
+        const projects = data.ourprojects || [];
+        const limitedProjects = projects.slice(0, 4);
         setProjects(limitedProjects);
       });
 
@@ -83,8 +85,11 @@ function ProjectsPage() {
                         <p className="text-gray-300 pb-14">{project.date}</p>
                         <div className="flex-grow relative w-full">
                           <Image
+                            width={500}
+                            height={300}
                             src={project.imgLogo}
                             alt={project.title}
+                            layout="intrinsic"
                             className="m-auto z-0 opacity-100 group-hover:scale-[1.07] duration-1000"
                           />
                         </div>
@@ -121,8 +126,12 @@ function ProjectsPage() {
                   </h3>
                   <p className="text-gray-300 pb-14">{project.date}</p>
                   <Image
+                    width={500}
+                    height={300}
                     src={project.imgLogo}
                     alt={project.title}
+                    layout="intrinsic"
+
                     className="inset-0 m-auto z-0 opacity-100 group-hover:scale-[1.07] duration-1000"
                   />
                   <button className="w-[150px] absolute bottom-4 left-1/2 transform -translate-x-1/2 text-white px-4 py-2 opacity-0 group-hover:opacity-100 transition ease-in-out bg-gradient-to-b from-black to-gray-800 duration-1000 rounded-[140px] flex items-center justify-center">
