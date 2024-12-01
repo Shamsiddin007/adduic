3dfe7610885e59ea21c6ccc96866f0403fdb4f79
Ramziddin <mahmudovramziddin25@gmail.com>
2024-12-01 16:16:19 +0500

    Update Our project

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -3,14 +3,15 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./style.module.css";
+import "./style.module.css"
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 
-import ProjectImg1 from "public/Imgs/partnersimg.png";
-import ProjectImg2 from "public/Imgs/partnersimg1.png";
-import ProjectImg3 from "public/Imgs/partnersimg2.png";
-import ProjectImg4 from "public/Imgs/partnersimg3.png";
-import ProjectImg5 from "public/Imgs/partnersimg4.png";
+import ProjectImg1 from "public/icons/Слой_x0020_1.png";
+import ProjectImg2 from "public/icons/Logo(2).png";
+import ProjectImg3 from "public/icons/Group 3(1).png";
+import ProjectImg4 from "public/icons/Logo(3).png";
+import ProjectImg5 from "public/icons/Logo(1).svg";
 
 import { Autoplay} from 'swiper/modules';
 
@@ -23,20 +24,19 @@ const Projects = () => {
       </h1>
 
       <Swiper
-        className="mx-auto max-w-[80%]"
+        className="m-auto w-[80%]"
         loop={true}
-        // autoplay={{
-        //   delay: 1500,
-        //   disableOnInteraction: false,
-        // }}
-        spaceBetween={16}
+        autoplay={{
+          delay: 1500,
+          disableOnInteraction: false,
+        }}
         modules={[Autoplay]}
         breakpoints={{
           1200: {
-            slidesPerView: 4,
+            slidesPerView: 5,
             spaceBetween: 20,
           },
-          800: {
+          850: {
             slidesPerView: 3,
             spaceBetween: 16,
           },
@@ -46,72 +46,72 @@ const Projects = () => {
           },
         }}
       >
-        <SwiperSlide>
-          <div className="rounded-[8px] w-[200px] hover:shadow-custom-blue-light-inset2 duration-1000 ease-in-out">
-            <Link href={"https://uzbekistans.club/"} target="blank">
-              <Image
-                src={ProjectImg1}
-                alt="project image"
-                className={styles.cardich}
-                width={200}
-                height={120}
-              />
-            </Link>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="rounded-[8px] w-[200px] hover:shadow-custom-blue-light-inset2 duration-1000 ease-in-out">
-            <Link href={"https://sqb.uz/uz/individuals/"} target="blank">
-              <Image
-                src={ProjectImg2}
-                alt="project image"
-                className={styles.cardich}
-                width={200}
-                height={120}
-              />
-            </Link>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="rounded-[8px] w-[200px] hover:shadow-custom-blue-light-inset2 duration-1000 ease-in-out">
-            <Link href={"https://imkon.uz/"} target="blank">
-              <Image
-                src={ProjectImg3}
-                alt="project image"
-                className={styles.cardich}
-                width={200}
-                height={120}
-              />
-            </Link>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="rounded-[8px] w-[200px] hover:shadow-custom-blue-light-inset2 duration-1000 ease-in-out">
-            <Link href={"https://uic.group/"} target="blank">
-              <Image
-                src={ProjectImg4}
-                alt="project image"
-                className={styles.cardich}
-                width={200}
-                height={120}
-
-              />
-            </Link>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="rounded-[8px] w-[200px] hover:shadow-custom-blue-light-inset2 duration-1000 ease-in-out">
-            <Link href={"https://paylov.uz/"} target="blank">
-              <Image
-                src={ProjectImg5}
-                alt="project image"
-                className={styles.cardich}
-                width={200}
-                height={120}
-              />
-            </Link>
-          </div>
-        </SwiperSlide>
+          <SwiperSlide className="sm:w-[200px] w-[150px]">
+            <div className="w-[150px] border-gray-700 border-[1px] sm:w-[181px] h-[112px] rounded-lg  flex items-center justify-center bg-[#FFFFFF0A] hover:shadow-custom-blue-light-inset2 duration-1000 ease-in-out">
+              <Link href={"https://uzbekistans.club/"} target="blank">
+                <Image
+                  src={ProjectImg1}
+                  alt="project image"
+                  className="object-cover w-[114px] h-[46px]"
+                />
+              </Link>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="sm:w-[200px] w-[150px]">
+            <div className="w-[150px] border-gray-700 border-[1px] sm:w-[181px] h-[112px] rounded-lg  flex items-center justify-center bg-[#FFFFFF0A] hover:shadow-custom-blue-light-inset2 duration-1000 ease-in-out">
+              <Link href={"https://sqb.uz/"} target="blank">
+                <Image
+                  src={ProjectImg2}
+                  alt="project image"
+                  className="object-cover w-[114px] h-[41px]"
+                />
+              </Link>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="sm:w-[200px] w-[150px]">
+            <div className="w-[150px] border-gray-700 border-[1px] sm:w-[181px] h-[112px] rounded-lg  flex items-center justify-center bg-[#FFFFFF0A] hover:shadow-custom-blue-light-inset2 duration-1000 ease-in-out">
+              <Link href={"https://it-market.uz/"} target="blank">
+                <Image
+                  src={ProjectImg3}
+                  alt="project image"
+                  className="object-cover w-[90px] h-[38px]"
+                />
+              </Link>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="sm:w-[200px] w-[150px]">
+            <div className="w-[150px] border-gray-700 border-[1px] sm:w-[181px] h-[112px] rounded-lg  flex items-center justify-center bg-[#FFFFFF0A] hover:shadow-custom-blue-light-inset2 duration-1000 ease-in-out">
+              <Link href={"https://uic.group"} target="blank">
+                <Image
+                  src={ProjectImg4}
+                  alt="project image"
+                  className="object-coverw-[144px] h-[29px]"
+                />
+              </Link> 
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="sm:w-[200px] w-[150px]">
+            <div className="w-[150px] border-gray-700 border-[1px] sm:w-[181px] h-[112px] rounded-lg  flex items-center justify-center bg-[#FFFFFF0A] hover:shadow-custom-blue-light-inset2 duration-1000 ease-in-out">
+              <Link href={"https://uzbekistans.club/"} target="blank">
+                <Image
+                  src={ProjectImg1}
+                  alt="project image"
+                  className="object-cover w-[114px] h-[46px]"
+                />
+              </Link>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="sm:w-[200px] w-[150px]">
+            <div className="w-[150px] border-gray-700 border-[1px] sm:w-[181px] h-[112px] rounded-lg  flex items-center justify-center bg-[#FFFFFF0A] hover:shadow-custom-blue-light-inset2 duration-1000 ease-in-out">
+              <Link href={"https://paylov.uz/"} target="blank">
+                <Image
+                  src={ProjectImg5}
+                  alt="project image"
+                  className="object-cover w-[118px] h-[38px]"
+                />
+              </Link>
+            </div>
+          </SwiperSlide>
       </Swiper>
     </div>
   );
