c3e7733be9adfac2a6ddd2a25333ec0d556552c6
Shamsiddin007 <shamsiddinruziboyev13@gmail.com>
2025-02-26 22:51:57 +0500

    pavtor

--- src/components/Footerlayout.tsx
+++ src/components/Footerlayout.tsx
@@ -1,8 +1,9 @@
-"use client"
+"use client";
 
 import Image from "next/image";
-import {Link} from "@/navigation";
+import { Link } from "@/navigation";
 import uiclogo from "/public/icons/Logo.svg";
+import IqLogos from "/public/icons/IqtidorLogosWhite.png";
 import phone from "/public/icons/phone.png";
 import email from "/public/icons/email.png";
 import telegram from "/public/icons/telegram.png";
@@ -10,35 +11,44 @@ import linkidin from "/public/icons/linkidin.png";
 import youTube from "/public/icons/youTube.png";
 import instagram from "/public/icons/instagram.png";
 import facebook from "/public/icons/facebook.png";
-import styles from "./style.module.css"
+import styles from "./style.module.css";
 import Forma from "@/app/[locale]/(root)/forma/forma";
 import { useTranslations } from "next-intl";
 const Footer = () => {
-
-  const t = useTranslations("FooterTitle") ;
+  const t = useTranslations("FooterTitle");
 
   const socialLinks = [
     { icon: linkidin, alt: "LinkedIn", href: "https://www.linkedin.com/" },
-    { icon: facebook, alt: "Facebook", href: "https://www.facebook.com/UICGames/"},
-    { icon: instagram, alt: "Instagram", href: "https://www.instagram.com/uicgames?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" },
+    {
+      icon: facebook,
+      alt: "Facebook",
+      href: "https://www.facebook.com/UICGames/",
+    },
+    {
+      icon: instagram,
+      alt: "Instagram",
+      href: "https://www.instagram.com/uicgames?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
+    },
     { icon: telegram, alt: "Telegram", href: "https://t.me/uicgames" },
     { icon: youTube, alt: "YouTube", href: "https://www.youtube.com/" },
   ];
 
   return (
-    <footer className="w-full relative h-max "
-    style={{
-      backgroundImage: "linear-gradient(0deg, #FFFFFF0A,transparent,transparent,transparent,transparent, rgb(16, 34, 179) 99%,transparent)"
-    }}
+    <footer
+      className="w-full relative h-max "
+      style={{
+        backgroundImage:
+          "linear-gradient(0deg, #FFFFFF0A,transparent,transparent,transparent,transparent, rgb(16, 34, 179) 99%,transparent)",
+      }}
     >
-    <div className="w-full flex justify-center items-center">
-      <div className={styles.form}>
-         <Forma />
-       </div>
-    </div>
+      <div className="w-full flex justify-center items-center">
+        <div className={styles.form}>
+          <Forma />
+        </div>
+      </div>
       <div className="relative pt-[280px] pb-12 px-4 sm:px-6 lg:px-8 z-3">
         <div
-          className="absolute right-0 bottom-0 w-1/2 h-[55%] bg-no-repeat bg-cover z-1"
+          className="absolute right-0 bottom-0 w-1/2 h-[55%] bg-no-repeat bg-cover z-0"
           style={{
             backgroundImage: "url('/icons/footerBgImg.png')",
             transformOrigin: "center",
@@ -89,7 +99,9 @@ const Footer = () => {
                     <Image src={phone} alt="Phone" className="w-5 h-5" />
                   </span>
                   <div>
-                    <p className="text-sm text-gray-400">{t("footer_phoneTitle")}</p>
+                    <p className="text-sm text-gray-400">
+                      {t("footer_phoneTitle")}
+                    </p>
                     <p className="text-white">+998 (71) 252-74-77</p>
                   </div>
                 </div>
@@ -98,20 +110,24 @@ const Footer = () => {
                     <Image src={email} alt="Email" className="w-5 h-5" />
                   </span>
                   <div>
-                    <p className="text-sm text-gray-400">{t("footer_emailTitle")}</p>
+                    <p className="text-sm text-gray-400">
+                      {t("footer_emailTitle")}
+                    </p>
                     <p className="text-white">info@uicgames.uz</p>
                   </div>
                 </div>
               </div>
             </div>
 
-
             {/* Right Column */}
             <div className="space-y-6">
               <h2 className="text-3xl sm:text-4xl font-bold text-white">
-                {t("footer_TitleLogo")} <span className="text-[#3375F6]">UIC GAMES!</span>
+                {t("footer_TitleLogo")}{" "}
+                <span className="text-[#3375F6]">UIC GAMES!</span>
               </h2>
-              <p className="text-gray-400 text-base sm:text-lg max-w-lg">{t("footer_description")} </p>
+              <p className="text-gray-400 text-base sm:text-lg max-w-lg">
+                {t("footer_description")}{" "}
+              </p>
 
               {/* Social Links */}
               <div className="flex gap-4">
@@ -132,6 +148,17 @@ const Footer = () => {
               </div>
             </div>
           </div>
+        </div>
+        <div className=" flex items-center justify-center">
+          <span className="text-base font-sans text-white">Development by</span>
+          <Image
+            src={IqLogos}
+            alt="IqLogos"
+            width={70}
+            height={70}
+            className="w-[100px] h-[100px] object-contain"
+          />
+          {/* <span>11</span> */}
         </div>
       </div>
     </footer>
